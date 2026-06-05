e10484334a35198fdd9635896600f6c42fccd07c
Aadrita <trish101202303@gmail.com>
2026-06-06 03:46:00 +0530

    Remove upload_model.py from repo

--- upload_model.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import upload_folder
-
-upload_folder(
-    folder_path='models/distilbert_sentiment',
-    repo_id='Aadritaray/review-lens-sentiment',
-    repo_type='model'
-)
-print('Upload complete')
